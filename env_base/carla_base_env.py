687ea0846ebe3bf2c983137a5c3f4ffdf31b6dcc
Osama <oy02945@st.habib.edu.pk>
2019-07-08 15:01:15 -0500

    Added custom tracks, implemented manual GYM env

--- env_base/carla_base_env.py
+++ env_base/carla_base_env.py
@@ -4,7 +4,35 @@ OpenAI Gym compatible Driving simulation environment based on Carla 0.9.5.
 Assumes a server instance to be running!
 """
 
-SERVER_PORT = 2000
+'''
+
+    W            : throttle
+    S            : brake
+    AD           : steer
+    Q            : toggle reverse
+    Space        : hand-brake
+    P            : toggle autopilot
+    M            : toggle manual transmission
+    ,/.          : gear up/down
+
+    TAB          : change sensor position
+    `            : next sensor
+    [1-9]        : change to sensor [1-9]
+    C            : change weather (Shift+C reverse)
+    Backspace    : change vehicle
+
+    R            : toggle recording images to disk
+
+    CTRL + R     : toggle recording of simulation (replacing any previous)
+    CTRL + P     : start replaying last recorded simulation
+    CTRL + +     : increments the start time of the replay by 1 second (+SHIFT = 10 seconds)
+    CTRL + -     : decrements the start time of the replay by 1 second (+SHIFT = 10 seconds)
+
+    F1           : toggle HUD
+    H/?          : toggle help
+    ESC          : quit
+
+'''
 
 # Find CARLA module
 
@@ -32,111 +60,60 @@ import traceback
 import time
 from datetime import datetime
 import random
+import client
+from client import *
+import pygame
 
 class CarlaEnv(gym.Env):
-	def __init__(self):
-		self.actor_list = []
+	def __init__(self,args):
+		self.args = args
 		self.client = None
-		self.blueprint_library = None
-		self.car = None #The car actor that we will control
-		self.init_client()
+		self.world = None
+		self.clock = None
+		self.display = None
+		self.hud = None
+		self.controller = None
+		self.start_client()
 
-	def init_client(self):
+	def start_client(self):
+		pygame.init()
+		pygame.font.init()
+		self.clock = pygame.time.Clock()
 		try:
-			# First of all, we need to create the client that will send the requests
-			# to the simulator. Here we'll assume the simulator is accepting
-			# requests in the localhost at port 2000.
-			self.client = carla.Client('localhost', SERVER_PORT)
+			self.client = carla.Client(self.args.host, self.args.port)
 			self.client.set_timeout(2.0)
-			# Once we have a client we can retrieve the world that is currently
-			# running.
-			world = self.client.get_world()
+
+			self.display = pygame.display.set_mode(
+				(self.args.width, self.args.height),
+				pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+			self.hud = HUD(self.args.width, self.args.height)
+			self.world = World(self.client.get_world(), self.hud, self.args.filter, self.args.rolename)
+			self.controller = KeyboardControl(self.world, self.args.autopilot)
+			self.clock.tick_busy_loop(60)
 		except ConnectionError:
-			print('Make sure server is up and running.')
-			# The world contains the list blueprints that we can use for adding new
-		# actors into the simulation.
-		self.blueprint_library = world.get_blueprint_library()
-
-		# Now let's filter all the blueprints of type 'vehicle' and choose one
-		# at random. 
-		# Change this choice to 1 vehicle passed through the cmd
-		
-		#print(self.blueprint_library.filter('vehicle'))
-		
-		bp = random.choice(self.blueprint_library.filter('vehicle'))
-
-		#print(bp)
-
-		# A blueprint contains the list of attributes that define a vehicle's
-		# instance, we can read them and modify some of them. For instance,
-		# let's randomize its color.
-		if bp.has_attribute('color'):
-			color = random.choice(bp.get_attribute('color').recommended_values)
-			bp.set_attribute('color', color)
-
-		# Now we need to give an initial transform to the vehicle. We choose a
-		# random transform from the list of recommended spawn points of the map.
-		transform = random.choice(world.get_map().get_spawn_points())
-
-		# So let's tell the world to spawn the vehicle.
-		self.car = world.spawn_actor(bp, transform)
-
-		# It is important to note that the actors we create won't be destroyed
-		# unless we call their "destroy" function. If we fail to call "destroy"
-		# they will stay in the simulation even after we quit the Python script.
-		# For that reason, we are storing all the actors we create so we can
-		# destroy them afterwards.
-		self.actor_list.append(self.car)
-		print('created %s' % self.car.type_id)
-
-		# Let's put the vehicle to drive around.
-		self.car.set_autopilot(True)
-
-		# Let's add now a "depth" camera attached to the vehicle. Note that the
-		# transform we give here is now relative to the vehicle.
-		camera_bp = self.blueprint_library.find('sensor.camera.depth')
-		camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
-		camera = world.spawn_actor(camera_bp, camera_transform, attach_to=self.car)
-		self.actor_list.append(camera)
-		print('created %s' % camera.type_id)
-
-		# Now we register the function that will be called each time the sensor
-		# receives an image. In this example we are saving the image to disk
-		# converting the pixels to gray-scale.
-		cc = carla.ColorConverter.LogarithmicDepth
-		camera.listen(lambda image: image.save_to_disk('_out/%06d.png' % image.frame_number, cc))
-
-		# Oh wait, I don't like the location we gave to the vehicle, I'm going
-		# to move it a bit forward.
-		location = self.car.get_location()
-		location.x += 40
-		self.car.set_location(location)
-		print('moved vehicle to %s' % location)
-
-
-
-		time.sleep(5)
-		self.clear()
+			print('Client couldn\'t connect properly, please retry.')
 
 	def reset(self):
-		pass
-
-
-	def reset_env(self):
-		pass
+		#reset all variables here
+		if (self.world):
+			self.world.restart()
 
 
 	def step(self,obs):
-		#print('Server version:',self.client.get_server_version())
+		if self.controller.parse_events(self.client, self.world, self.clock):
+			return
+		self.world.tick(self.clock)
+		self.world.render(self.display)
+		pygame.display.flip()
 		return 0,0,0,0
 
-	def clear(self):
-		print("Clearing created actors..")
-		for actor in self.actor_list:
-			actor.destroy()
-		print('Cleaned')
-
-
+	def __del__(self):
+		if (self.world and self.world.recording_enabled):
+			self.client.stop_recorder()
+		if self.world is not None:
+			self.world.destroy()
+		pygame.quit()
 
 def main():
 	argparser = argparse.ArgumentParser(
@@ -188,14 +165,13 @@ def main():
 	print(__doc__)
 
 	try:
-
 		rl_loop(args)
 
 	except KeyboardInterrupt:
 		print('\nCancelled by user. Bye!')
 
 def rl_loop(args):
-	env = CarlaEnv()
+	env = CarlaEnv(args)
 	obs = env.reset()
 	done = False
 	t = 0
@@ -204,7 +180,8 @@ def rl_loop(args):
 		t += 1
 		obs, reward, done, info = env.step([1.0, 0.0])  # Full throttle, zero steering angle
 		total_reward += reward
-		print("step#:", t, "reward:", round(reward, 4), "total_reward:", round(total_reward, 4), "done:", done)
+		#print("step#:", t, "reward:", round(reward, 4), "total_reward:", round(total_reward, 4), "done:", done)
+		#break
 
 if __name__ == "__main__":
 	main()
